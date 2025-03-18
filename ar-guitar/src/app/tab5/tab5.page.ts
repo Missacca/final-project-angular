15d358da7ebf22e45e70de5998462474e8ac4ac4
HandlerOne <107484116+Missacca@users.noreply.github.com>
2025-03-18 23:28:26 +0800

    login page

--- ar-guitar/src/app/tab5/tab5.page.ts
+++ ar-guitar/src/app/tab5/tab5.page.ts
@@ -9,6 +9,38 @@ import { Component} from '@angular/core';
 export class Tab5Page {
 
   constructor() { }
+  showLogin = true;
+  loginData = {
+    email: '',
+    password: '',
+  };
+  registerData = {
+    name: '',
+    email: '',
+    password: '',
+    repassword: '',
+  };
 
+  switchToLogin() {
+    this.showLogin = true;
+  }
+
+  switchToRegister() {
+    this.showLogin = false;
+  }
+
+  onLogin() {
+    console.log('Logging in with', this.loginData);
+    // Handle the login logic here
+  }
+
+  onRegister() {
+    if(this.registerData.repassword != this.registerData.password) {
+      alert("The password is entered inconsistently");
+    }else {
+      console.log('Registering with', this.registerData);
+    }
+    
+  }
 
 }
