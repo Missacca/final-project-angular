6dfa76045410a3b8fc19a207b8daee8d720e1814
HandlerOne <107484116+Missacca@users.noreply.github.com>
2025-05-11 19:04:51 +0800

    delete blank angular item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  standalone: false,
+
 })
 export class AppComponent {
-  title = 'my-app';
+  constructor() {}
 }
